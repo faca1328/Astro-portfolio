89dbfa6103edd3ccfc5a022bff47bdb29c07d62f
Facu M <facundoivanmartinez@hotmail.com>
2023-12-27 17:43:05 -0300

    second commit / starting the page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	content: [
+		"./index.html",
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+	  ],
+	  theme: {
+		screens: {
+	  
+		  'sm': '620px',
+	
+		  'md': '910px'
+		},
+		extend: {
+		  screens: {
+			'phone': '440px',
+			
+		  },
+		  keyframes: {
+			bounce: {
+			  '0%, 100%': { transform: 'translate3d(0,0,0)' },
+			  '70%': { transform: 'translate3d(0, -10px, 0)' },
+			  '90%': { transform: 'translate3d(0, -5px, 0)' },
+	
+			},
+		  },
+		  animation: {
+			'bounce-arrow': 'bounce 1s linear 2',
+		  },
+		},
+	  },
 	plugins: [],
 }
